5feb6a50fead71ed1f0418e65ab20aabba61f3c8
Aniket Bhosale <aniket15b@gmail.com>
2019-07-19 17:08:16 +0530

    Added infinite scroll to index

--- Frontend/src/app/index/index.page.ts
+++ Frontend/src/app/index/index.page.ts
@@ -5,16 +5,27 @@ import { LogoutService } from '../services/logout.service';
 import { MenuController } from '@ionic/angular';
 import { SubscriptionService } from '../services/subscription.service';
 
+import { ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
 })
 export class IndexPage implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   async menuControllerSwitch(){
     await this.menuController.enable(false,'homeList');
     await this.menuController.enable(true,'userList');
+  }
+
+  subs;
+  slice=3;
+
+  async subscriptions(){
+    return await this.subscriptionService.getsubs().toPromise();
   }
 
   constructor(private platform: Platform,
@@ -24,6 +35,20 @@ export class IndexPage implements OnInit {
             ){
 
       this.menuControllerSwitch();
+      this.subs = this.subscriptions();
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      this.slice += 1;
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (false) {
+        event.target.disabled = true;
+      }
+    }, 500);
   }
 
   ngOnInit() {
